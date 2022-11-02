c2f60fd891eb4926c7fecc2015dfc2a091d29a7c
LasseK <lkr97km@hotmail.dk>
2022-11-02 01:35:29 +0100

    Script changes

--- script.js
+++ script.js
@@ -10,13 +10,20 @@ window.addEventListener('load', () => {
             fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1b34bbea396b3a187b04eeb242b957e9&units=metric`)
                 .then(res => res.json())
                 .then(data => {
+
+                    const land = { DK: 'Danmark', NO: 'Norge', SE: 'Sverige' }
                     console.log(data)
-                    const { temp } = data.main
+                    const { temp, temp_max, temp_min } = data.main
+                    const { country } = data.sys
                     const { description, main } = data.weather[0]
 
                     const weatherCondition = 'assets/' + String(main).toLowerCase() + '.json';
 
+                    $('.currentLocation').innerHTML = data.name + ', ' + land[`${country}`];
+
                     $(".currentTemp").innerHTML = Math.round(temp) + "°C";
+                    $(".currentMaxTemp").innerHTML = Math.round(temp_max) + "°C";
+                    $(".currentMinTemp").innerHTML = Math.round(temp_min) + "°C";
                 })
         })
     }
