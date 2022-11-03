68d0fc56711d93c46dd37702b0dbf87cc2152a1a
LasseK <lkr97km@hotmail.dk>
2022-11-03 18:47:56 +0100

    lastest version

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+const accuweather_api_key = 'iGz1SaKzuE4IvdmTBCzMjPBs69KGINlb'
+
 const $ = (input) => {
     return document.querySelector(input);
 }
+
+// Lottiefile
+const lottie = $('.lottieWeather');
+
+function setIcon(iconName) {
+    let weatherAni = bodymovin.loadAnimation({
+        wrapper: lottie,
+        animType: 'svg',
+        loop: true,
+        autoplay: true,
+        path: `assets/${iconName}.json`
+    });
+    weatherAni.setSpeed(0.5);
+    weatherAni.play();
+}
+let firstData;
+
 window.addEventListener('load', () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             let lat = position.coords.latitude;
             let long = position.coords.longitude;
-            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1b34bbea396b3a187b04eeb242b957e9&units=metric`)
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=1b34bbea396b3a187b04eeb242b957e9&units=metric`)
                 .then(res => res.json())
                 .then(data => {
-                    changeWeatherData(data)
+                    let cityName = data.city.name;
+                    let country = data.city.country;
+                    changeWeatherData(data, cityName, country)
                 })
-
         })
     }
 })
@@ -34,10 +54,9 @@ const updateSearch = debounceRequest((text) => {
         $(".searchResults").style.opacity = "0";
         return
     }
-    fetch(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=cXgSSt7gILstdry1Az2yBRg2JHG8ryEN&q=${text}`)
+    fetch(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${accuweather_api_key}&q=${text}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
             $(".searchResults").style.opacity = "1";
             $(".searchResults").innerHTML = "";
             $('.searchResults').style.transform = "scaleY(1)";
@@ -45,13 +64,13 @@ const updateSearch = debounceRequest((text) => {
                 let li = document.createElement('li');
                 li.classList.add('resultItem');
                 li.dataset.by = `${data[i].LocalizedName}`;
+                li.dataset.location = `${data[i].Key}`;
                 li.innerText = `${data[i].LocalizedName}, ${data[i].Country.LocalizedName}`;
-
                 // Eventlistener
-                li.addEventListener('click', e => {
+                li.addEventListener('click', (e) => {
                     $(".searchResults").innerHTML = "";
                     $(".searchResults").style.opacity = "0";
-                    fetchNewCity(e.target.dataset.by)
+                    fetchNewCity(e.target.dataset.by, e.target.dataset.location)
                     $('.searchTool').value = "";
                 })
 
@@ -65,32 +84,67 @@ $(".searchTool").addEventListener('input', e => {
     updateSearch(e.target.value);
 })
 
-const fetchNewCity = (city) => {
+const fetchNewCity = (city, locationKey) => {
+
+    fetch(`http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${locationKey}?apikey=${accuweather_api_key}&metric=true`)
+        .then(res => res.json())
+        .then(forecast => {
+            console.log(forecast)
+            for (let i = 0; i < forecast.length; i++) {
+                let span = document.createElement('span');
+                let firstP = document.createElement('p');
+                let secondP = document.createElement('p');
+                let img = document.createElement('img');
+
+
+                span.classList.add('timeKort');
+                firstP.classList.add('timeKortTitel');
+                secondP.classList.add('timeKortTid');
+                img.classList.add('timeKortIkon');
+
+                img.src = `assets/png/${forecast[i].IconPhrase.split(' ').join('')}.png`
+
+                firstP.innerText = forecast[i].Temperature.Value + '°C';
+                secondP.innerText = forecast[i].DateTime.split('').splice(11, 5).join('')
+
+                span.append(firstP, img, secondP);
+
+                $('.timeKortBeholder').appendChild(span);
+            }
+        })
+
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=1b34bbea396b3a187b04eeb242b957e9`)
         .then(res => res.json())
         .then(data => {
-            console.log('Booty')
-
-            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1b34bbea396b3a187b04eeb242b957e9&units=metric`)
+            let placeName = data[0].name;
+            let country = data[0].country;
+            let lat = data[0].lat;
+            let long = data[0].lon;
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=1b34bbea396b3a187b04eeb242b957e9&units=metric`)
                 .then(res => res.json())
                 .then(data => {
-                    changeWeatherData(data)
+                    changeWeatherData(data, placeName, country)
                 })
 
         })
 }
 
-const changeWeatherData = (data) => {
-    const land = { DK: 'Danmark', NO: 'Norge', SE: 'Sverige' }
+const changeWeatherData = (weather, place, country) => {
+    let data = weather.list[0];
     console.log(data)
-    const { humidity, temp, temp_max, temp_min } = data.main
+
+    $('.lottieWeather').innerHTML = "";
+    let currentIcon = data.weather[0].main.split(' ').join('').toLowerCase();
+    setIcon(currentIcon);
+
+    const land = { DK: 'Danmark', NO: 'Norge', SE: 'Sverige' }
+    const { feels_like, humidity, temp, temp_max, temp_min } = data.main
     const { deg, speed } = data.wind
-    const { country } = data.sys
     const { description, main } = data.weather[0]
 
     const weatherCondition = 'assets/' + String(main).toLowerCase() + '.json';
 
-    $('.currentLocation').innerHTML = data.name + ', ' + land[`${country}`];
+    $('.currentLocation').innerHTML = place + ', ' + land[`${country}`];
 
     $(".currentTemp").innerHTML = Math.round(temp) + "°C";
     $(".currentMaxTemp").innerHTML = Math.round(temp_max) + "°C";
@@ -101,13 +155,25 @@ const changeWeatherData = (data) => {
     $(':root').style.setProperty('--wind-deg', `${deg}deg`)
     $('.windDescription').innerHTML = deg + ' °';
 
+    $('.feelsValue').innerHTML = feels_like + "°C";
+
     //Luftfugtighed
     $('.fugtValue').innerHTML = humidity + ' g/m³';
+
+    //regn
+    $('.regnChance').innerHTML = Math.round(data.pop * 100) + '%'
 }
 
+let listValues;
 
-$('.searchTool').addEventListener("focusout", () => {
-    $('.searchTool').value = "";
-    $(".searchResults").innerHTML = "";
+$('.searchTool').addEventListener("focusout", (e) => {
+    listValues = $(".searchResults").innerHTML;
+    setTimeout(() => {
+        $(".searchResults").innerHTML = "";
+    }, 200)
+});
+
+$('.searchTool').addEventListener("focus", () => {
+    $(".searchResults").innerHTML = listValues;
 });
 
